3fd0ac81789f4b4f4e1341a0ef4a15171c95dffe
ARahmanLskit <arahman@lskit.com>
2023-08-02 11:57:25 +0600

    first initial bootstrap setup

---
+++ app/page.js
@@ -0,0 +1,14 @@
+"use client";
+import Image from "next/image";
+import styles from "./page.module.css";
+import Button from "react-bootstrap/Button";
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <Button>Hello</Button>
+      </div>
+    </>
+  );
+}
